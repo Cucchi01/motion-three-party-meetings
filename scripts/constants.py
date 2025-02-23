6d4ad74948a5f79daaa851d84fa4ebcb01bee288
Cucchi01 <cucchi01centr@gmail.com>
2025-02-23 13:34:45 +0100

    [ADD] compute and save motions for all videos

--- scripts/constants.py
+++ scripts/constants.py
@@ -9,9 +9,21 @@ while CURRENT_PATH.absolute().parts[-1] != "motion-three-party-meetings":
 
 FOLDER_VIDEOS = Path(*CURRENT_PATH.absolute().parts, "data", "videos")
 FOLDER_FEATURES = Path(*CURRENT_PATH.absolute().parts, "data", "features")
+FOLDER_FEATURES_MOTION = FOLDER_FEATURES.joinpath("motions")
 
 SHAPE_PREDICTOR_PATH = Path(
     *CURRENT_PATH.absolute().parts, "weights", "shape_predictor_68_face_landmarks.dat"
 )
 
 USE_RAFT_LARGE = False
+
+
+class OpticalFlowConstansts:
+    PADDING_BOX_BOTTOM_FACE = 10
+    VISUALIZE = False
+    VISUALIZE_EVERY_NUM_FRAMES = 30
+    # do not consider all the frames to lower the amount of computations
+    KEEP_EVERY_NUM_FRAMES = 10
+    DOWNSAMPLE_RATE = 4
+
+    WINDOW_LENGTH = 2.0
--- scripts/optical_flow.py
+++ scripts/optical_flow.py
@@ -1,126 +1,193 @@
+from genericpath import isfile
+import os
 import constants
+from constants import FOLDER_VIDEOS, OpticalFlowConstansts
 import cv2
 import dlib
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from tqdm import tqdm
+from pathlib import Path
+import datetime
+import timeit
 
 # face detector able to detect faces that are looking more or less to the camera
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(str(constants.SHAPE_PREDICTOR_PATH))
 
+
 # Open video file
-path_video = str(constants.FOLDER_VIDEOS) + "/video_test.mp4"
+def compute_motions(
+    path: Path, current_num_file: int, tot_num_videos: int, start_time
+) -> int:
+    if path.is_dir():
+        # scan the folder recursively
+        for file in path.iterdir():
+            current_num_file = compute_motions(
+                file, current_num_file, tot_num_videos, start_time
+            )
+        return current_num_file
 
-cap = cv2.VideoCapture(path_video)
-fps = cap.get(cv2.CAP_PROP_FPS)
-num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-print(f"FPS: {fps}; Num frames: {num_frames}; duration: {num_frames/fps:.2f} s")
+    current_num_file += 1
 
-list_magnitudes = []
+    # load video and metadata
+    cap = cv2.VideoCapture(str(path))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    if current_num_file == 1:
+        print(f"File: {path.name}; File number 1 of {tot_num_videos}")
+    else:
+        estimated_time_left = (
+            (tot_num_videos - current_num_file + 1)
+            * (timeit.default_timer() - start)
+            / (current_num_file - 1)
+        )
+        print(
+            f"File: {path.name}; File number {current_num_file} of {tot_num_videos}; Estimated time left:{datetime.timedelta(seconds=(int(estimated_time_left)))}"
+        )
+    print(f"FPS: {fps}; Num frames: {num_frames}; duration: {num_frames/fps:.2f} s")
 
-visualize = False
-visualize_every_num_frames = 30
-# do not consider all the frames to lower the amount of computations
-keep_every_num_frame = 10
-downsample_rate = 4
+    list_magnitudes = []
 
-# Read the first frame and convert to grayscale
-ret, prev_frame = cap.read()
-prev_frame = cv2.GaussianBlur(
-    prev_frame, (5, 5), 1 / (2 * downsample_rate)
-)  # kernel size and standard deviation
-prev_frame = cv2.resize(
-    prev_frame,
-    (prev_frame.shape[1] // downsample_rate, prev_frame.shape[0] // downsample_rate),
-)
-prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-
-
-for num_frame in tqdm(range(0, int(num_frames), keep_every_num_frame)):
-    if not cap.isOpened():
-        break
-
-    # set the position to the next frame
-    cap.set(cv2.CAP_PROP_POS_FRAMES, num_frame)
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    # smooth and downsample
-    frame = cv2.GaussianBlur(
-        frame, (5, 5), 1 / (2 * downsample_rate)
+    # Read the first frame and convert to grayscale
+    ret, prev_frame = cap.read()
+    print(f"Quality {prev_frame.shape[0]} X {prev_frame.shape[1]}")
+    prev_frame = cv2.GaussianBlur(
+        prev_frame, (5, 5), 1 / (2 * OpticalFlowConstansts.DOWNSAMPLE_RATE)
     )  # kernel size and standard deviation
-    frame = cv2.resize(
-        frame, (frame.shape[1] // downsample_rate, frame.shape[0] // downsample_rate)
+    prev_frame = cv2.resize(
+        prev_frame,
+        (
+            prev_frame.shape[1] // OpticalFlowConstansts.DOWNSAMPLE_RATE,
+            prev_frame.shape[0] // OpticalFlowConstansts.DOWNSAMPLE_RATE,
+        ),
     )
+    prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
 
-    # Convert to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # process the remaining frames
+    for num_frame in tqdm(
+        range(1, int(num_frames), OpticalFlowConstansts.KEEP_EVERY_NUM_FRAMES)
+    ):
+        if not cap.isOpened():
+            break
 
-    # Compute optical flow (Farneback method)
-    flow = cv2.calcOpticalFlowFarneback(
-        prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0
-    )  # (prev, next, ., pyramid_scale, num_pyramid_layers, average_window_size, num_pixel_neighborhood_poly_expansion, std_related_to_num_pixel_neigh, flags), pyramid_scale =0.5 is classical pyramid. High average window size get robust results, but more blurred
-    magnitude = cv2.magnitude(flow[..., 0], flow[..., 1])
+        # set the position to the next frame
+        cap.set(cv2.CAP_PROP_POS_FRAMES, num_frame)
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # Detect faces
-    faces = detector(gray)
-    for face in faces:
-        # Get facial landmarks
-        landmarks = predictor(gray, face)
-
-        # Mouth coordinates
-        mouth_x1 = landmarks.part(4).x
-        mouth_y1 = landmarks.part(51).y
-        mouth_x2 = landmarks.part(12).x
-        mouth_y2 = landmarks.part(8).y
-
-        # Expand box slightly
-        padding = 10
-        mouth_x1 -= padding
-        mouth_y1 -= padding
-        mouth_x2 += padding
-        mouth_y2 += padding
-
-        # Draw black box over mouth
-        cv2.rectangle(
-            magnitude, (mouth_x1, mouth_y1), (mouth_x2, mouth_y2), (0, 0, 0), -1
+        # smooth and downsample
+        frame = cv2.GaussianBlur(
+            frame, (5, 5), 1 / (2 * OpticalFlowConstansts.DOWNSAMPLE_RATE)
+        )  # kernel size and standard deviation
+        frame = cv2.resize(
+            frame,
+            (
+                frame.shape[1] // OpticalFlowConstansts.DOWNSAMPLE_RATE,
+                frame.shape[0] // OpticalFlowConstansts.DOWNSAMPLE_RATE,
+            ),
         )
 
-    list_magnitudes.append(np.average(magnitude))
+        # Convert to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # show output
-    if visualize and num_frame % visualize_every_num_frames == 0:
-        print("Average magnitude: ", list_magnitudes[-1])
-        fig, ax = plt.subplots()
-        ax.imshow(magnitude)
-        plt.show()
+        # Compute optical flow (Farneback method)
+        flow = cv2.calcOpticalFlowFarneback(
+            prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0
+        )  # (prev, next, ., pyramid_scale, num_pyramid_layers, average_window_size, num_pixel_neighborhood_poly_expansion, std_related_to_num_pixel_neigh, flags), pyramid_scale =0.5 is classical pyramid. High average window size get robust results, but more blurred
+        magnitude = cv2.magnitude(flow[..., 0], flow[..., 1])
 
-    # update previous frame
-    prev_gray = gray
+        # Detect faces
+        faces = detector(gray)
+        for face in faces:
+            # Get facial landmarks
+            landmarks = predictor(gray, face)
 
-cap.release()
+            # Mouth coordinates
+            mouth_x1 = landmarks.part(4).x
+            mouth_y1 = landmarks.part(51).y
+            mouth_x2 = landmarks.part(12).x
+            mouth_y2 = landmarks.part(8).y
 
-# computer average motion for each window
+            # Expand box
+            padding = OpticalFlowConstansts.PADDING_BOX_BOTTOM_FACE
+            mouth_x1 -= padding
+            mouth_y1 -= padding
+            mouth_x2 += padding
+            mouth_y2 += padding
 
-# length in seconds seconds
-window_length = 5
-num_frames_per_window = int(window_length * fps / keep_every_num_frame)
+            # Draw black box over mouth
+            cv2.rectangle(
+                magnitude, (mouth_x1, mouth_y1), (mouth_x2, mouth_y2), (0, 0, 0), -1
+            )
 
-magnitudes = np.array(list_magnitudes)
+        list_magnitudes.append(np.average(magnitude))
 
-print(np.insert(magnitudes, 0, 0))
-cumsums = np.cumsum(np.insert(magnitudes, 0, 0))
-averages = (cumsums[num_frames_per_window:] - cumsums[:-num_frames_per_window]) / float(
-    num_frames_per_window
+        # show output
+        if (
+            OpticalFlowConstansts.VISUALIZE
+            and num_frame % OpticalFlowConstansts.VISUALIZE_EVERY_NUM_FRAMES == 0
+        ):
+            print("Average magnitude: ", list_magnitudes[-1])
+            fig, ax = plt.subplots()
+            ax.imshow(magnitude)
+            plt.show()
+
+        # update previous frame
+        prev_gray = gray
+
+    cap.release()
+
+    # computer average motion for each window
+
+    # length in seconds seconds
+    window_length = OpticalFlowConstansts.WINDOW_LENGTH
+    num_frames_per_window = int(
+        window_length * fps / OpticalFlowConstansts.KEEP_EVERY_NUM_FRAMES
+    )
+
+    magnitudes = np.array(list_magnitudes)
+
+    cumsums = np.cumsum(np.insert(magnitudes, 0, 0))
+    averages = (
+        cumsums[num_frames_per_window:] - cumsums[:-num_frames_per_window]
+    ) / float(num_frames_per_window)
+    averages = averages[::num_frames_per_window]
+
+    # add last partial window
+    if (magnitudes.shape[0]) % num_frames_per_window != 0:
+        start_pos = averages.shape[0] * num_frames_per_window
+        averages = np.hstack([averages, np.average(magnitudes[start_pos:])])
+
+    timestamps = np.arange(averages.shape[0]) * window_length
+
+    data = pd.DataFrame()
+    data["Averages"] = averages
+    data["Timestamp"] = timestamps
+
+    # save the data
+    relative_pos_index = path.parts.index(FOLDER_VIDEOS.name)
+    feature_path = constants.FOLDER_FEATURES_MOTION.joinpath(
+        *path.parts[relative_pos_index + 1 :]
+    )
+    if not feature_path.parent.exists():
+        os.makedirs(feature_path.parent)
+    feature_path = feature_path.with_suffix(".csv")
+    data.to_csv(feature_path, index=False)
+
+    return current_num_file
+
+
+total_number_videos = sum(
+    len(files) for _, _, files in os.walk(constants.FOLDER_VIDEOS)
 )
-print(averages)
-averages = averages[::num_frames_per_window]
-timestamps = np.arange(averages.shape[0]) * window_length
-
-data = pd.DataFrame()
-data["Averages"] = averages
-data["Timestamp"] = timestamps
-print(data)
+print(f"Total number of videos: {total_number_videos}")
+start = timeit.default_timer()
+compute_motions(
+    constants.FOLDER_VIDEOS,
+    current_num_file=0,
+    tot_num_videos=total_number_videos,
+    start_time=start,
+)
